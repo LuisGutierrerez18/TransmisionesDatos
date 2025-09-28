c9f7c9427321fdd3ef46e38103fae2f35327e201
Luis Gutierrez <A00837481@tec.mx>
2025-09-28 15:06:09 -0600

    Add initial files and main.cpp skeleton
    Added transmission and malicious code sample text files, along with a skeleton for main.cpp. The C++ file sets up file names and outlines the structure for transmission analysis, palindrome search, and common substring search.

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+using namespace std;
+
+
+
+int main() {
+    // Leer archivos de transmisión y código malicioso
+    std:: string transmission1 = "transmission1.txt";
+    std:: string transmission2 = "transmission2.txt";
+    std:: string mcode1 = "mcode1.txt";
+    std:: string mcode2 = "mcode2.txt";
+    std:: string mcode3 = "mcode3.txt";
+
+    // Parte 1: Archivos de transmisión (6 busquedas)
+
+    // Parte 2: Búsqueda de palíndromos (2 busquedas)
+
+
+    // Parte 3: Busqueda de substrigs mas comun
+
+    return 0;
+}
